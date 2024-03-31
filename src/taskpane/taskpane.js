e38a356ab40a41acd6cdaa9b11fd584181a70d99
Kenya Chan <kenya.chan@gmail.com>
2024-03-31 16:07:56 +1100

    Add function to create SRDET sheet

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -6,10 +6,41 @@
 /* global console, document, Excel, Office */
 
 Office.onReady((info) => {
-  if (info.host === Office.HostType.Excel) {
-    document.getElementById("app-body").style.display = "flex";
-  }
+	if (info.host === Office.HostType.Excel) {
+		document.querySelector("#create-extraction-sheet").addEventListener(
+			'click', () => tryCatch(createDataTable)
+		);
+
+		document.getElementById("app-body").style.display = "flex";
+	}
 });
+
+async function createDataTable() {
+	await Excel.run(async (context) => {
+		const srdetSheet = createWorksheet(context);
+
+		createRosterExtractionTable(srdetSheet);
+		srdetSheet.activate();
+
+		await context.sync();
+	});
+
+	function createWorksheet(context) {
+		const wSheetName = 'SRDET';
+		const worksheet = context.workbook.worksheets.add(wSheetName);
+
+		return worksheet;
+	}
+
+	function createRosterExtractionTable(worksheet) {
+		const dataTable = worksheet.tables.add("A1:H2", true);
+		dataTable.name = "srdet";
+
+		dataTable.getHeaderRowRange().values =		
+			[["Name","Date","Start","End","Time","OT","Value","Address"]];
+			
+	}
+}
 
 async function tryCatch(callback) {
 	try {
