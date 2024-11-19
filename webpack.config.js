6fe6c485d780c444a5d3d801517d92454b3cdd20
Kenya Chan <kenya.chan@gmail.com>
2024-11-19 13:51:40 +1100

    Update production URL
    Update production URL to the GitHub Pages hosted site

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://uts-itd.github.io/SET-Roster-Data-Extraction-Tool/";
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
